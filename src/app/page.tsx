a3fd2ae476b49f6cd719ab32ff48ba78bf68a358
anay-208 <me@anayparaswani.dev>
2026-06-13 01:44:26 +0530

    UI for home page desktop + mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,126 @@
-import Image from "next/image";
+"use client"
+
+import dynamic from "next/dynamic"
+import { useState } from "react"
+
+import {
+  Field,
+  FieldDescription,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Button } from "@/components/ui/button"
+
+const Calendar = dynamic(
+  () => import("@/components/ui/calendar").then((mod) => mod.Calendar),
+  { ssr: false }
+)
+
+const timeOptions = Array.from({ length: 24 }, (_, hour) => {
+  const period = hour < 12 ? "AM" : "PM"
+  const displayHour = hour % 12 === 0 ? 12 : hour % 12
+
+  return {
+    label: `${displayHour}:00 ${period}`,
+    value: hour.toString(),
+  }
+})
+
+function getToOptions(fromValue: string) {
+  if (!fromValue) {
+    return timeOptions
+  }
+
+  const fromIndex = Number(fromValue)
+
+  return [...timeOptions.slice(fromIndex + 1), timeOptions[0]]
+}
 
 export default function Home() {
+  const [fromTime, setFromTime] = useState("")
+  const [toTime, setToTime] = useState("")
+  const [availableDates, setAvailableDates] = useState<Date[] | undefined>([])
+  const toOptions = getToOptions(fromTime)
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image 
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+    <main className="w-full max-w-4xl mx-auto pt-10 px-4">
+      <h1 className="text-4xl font-bold text-center mt-10  font-sans">Find the perfect time for your meeting</h1>
+
+      <div className="w-full bg-white rounded-2xl border border-slate-100 p-4 mt-12 font-mono flex flex-col md:flex-row gap-6 md:gap-12">
+      {/* Form for event name and show times */}
+        <div className="space-y-4 md:flex-3">
+          <Field>
+            <FieldLabel  htmlFor="event-name">Event Name</FieldLabel>
+            <Input
+              id="event-name"
+              type="text"
+              placeholder="Meeting"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </Field>
+          <Field>
+            <FieldLabel htmlFor="show-times">Show Times</FieldLabel>
+            <div className="flex flex-col gap-2 md:flex-row md:gap-4">
+              
+              <Select value={fromTime} onValueChange={(value) => {
+                setFromTime(value)
+                setToTime("")
+              }}>
+                <SelectTrigger className="w-[180px]" id="show-times-from">
+                  <SelectValue placeholder="From" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    {timeOptions.map((time) => (
+                      <SelectItem key={time.value} value={time.value}>{time.label}</SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <Select value={toTime} onValueChange={setToTime} disabled={!fromTime}>
+                <SelectTrigger className="w-[180px]" id="show-times-to">
+                  <SelectValue placeholder="To" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    {toOptions.map((time) => (
+                      <SelectItem key={time.value} value={time.value}>{time.label}</SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+          </Field>
+          <Button className="mt-2" size={"lg"} disabled={!fromTime || !toTime}>Create Event</Button>
         </div>
-      </main>
-    </div>
+
+
+        {/* Available Dates */}
+        <div className="md:flex-2">
+          <Field>
+            <FieldLabel className="text-sm text-slate-500">Available Dates</FieldLabel>
+            <FieldDescription className="text-xs text-slate-400">
+              Select one or more dates when this time range should be available.
+            </FieldDescription>
+            <div className="rounded-xl border border-slate-200 bg-slate-50 p-3">
+              <Calendar
+                mode="multiple"
+                selected={availableDates}
+                onSelect={setAvailableDates}
+                className="mx-auto w-3/4 md:w-full"
+              />
+            </div>
+          </Field>
+        </div>
+
+      </div>
+    </main>
   );
 }
